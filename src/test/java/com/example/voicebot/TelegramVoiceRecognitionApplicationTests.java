6aa5312919ea76f479c8a9c3f76a99ff7576b000
Artem <spambox@eeel.ru>
2022-10-08 23:40:22 +0700

    Initialization

---
+++ src/test/java/com/example/voicebot/TelegramVoiceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voicebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramVoiceRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
